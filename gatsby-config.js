700b07ff67b7e5dc74cbc19a6604a96e8e26d45a
Bart Wijnants <bart.w@sparkcentral.com>
2019-12-20 21:08:39 +0100

    graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  siteMetadata: {
+    title: `My Website`,
+    siteUrl: `https://my.web.site`,
+    description: `The best website in the world`
+  },
   plugins: [`gatsby-plugin-react-helmet`]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { useSiteMetadata } from "../hooks/use-site-metadata";
 
-export default () => (
-  <>
-    <Helmet>
-      <title>My Website</title>
-      <meta name="description" content="The description of my website" />
-    </Helmet>
-    <div>Hello world!</div>
-  </>
-);
+export default () => {
+  const { title, description } = useSiteMetadata();
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <h1>{title}</h1>
+      <div>Hello world!</div>
+    </>
+  );
+};
